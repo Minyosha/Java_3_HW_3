ef6f42fa6158eafc4e93214d74d7be29521bbc2b
Minyosha <93713454+Minyosha@users.noreply.github.com>
2022-04-03 20:04:30 +0400

    Delete ServerCommandConstants.java

--- src/com/geekbrains/server/ServerCommandConstants.java
+++
@@ -1,9 +0,0 @@
-package com.geekbrains.server;
-
-public class ServerCommandConstants {
-    public static final String AUTHENTICATION = "/auth";
-    public static final String EXIT = "/exit";
-    public static final String CLIENTS = "/clients";
-    public static final String PRIVATE_MESSAGE = "/w";
-    public static final String JOIN = "/join";
-}
